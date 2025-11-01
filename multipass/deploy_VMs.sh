6bae59d1e118c4d7259fa0ec3ac47432f6d3a3fd
Nguyen Manh Thanh <mthnhng@mthnhng.local>
2025-11-01 18:21:06 +0700

    Update booting VMs script and ignore tmp files

--- multipass/deploy_VMs.sh
+++ multipass/deploy_VMs.sh
@@ -3,12 +3,24 @@
 
 set -euo pipefail
 
+# Setting colors
+RED="\033[1;31m"
+YELLOW="\033[1;33m"
+GREEN="\033[1;32m"
+BLUE="\033[1;34m"
+NC="\033[0m"
+
+
+# Define VMs specs
 vm_specs=/tmp/vm_specs
 cat <<EOF > $vm_specs
 controlplane01,2,2G,10G
 controlplane02,1,2G,5G
 node01,2,4G,20G
 ingress,1,512M,5G
+EOF
+
+echo -e "${GREEN}Booting the nodes...${NC}"
 
 # Boot the nodes
 for vm in $(cat $vm_specs)
@@ -18,7 +30,43 @@ do
   ram=$(cut -d ',' -f 3 <<< $vm_specs)
   disk=$(cut -d ',' -f 4 <<< $vm_specs)
 
+  # Checking if there are any running VMs, if yes, delete them
+  if multipass list --format json | jq -r "list[].name" | grep $(cut -d ',' -f 1 <<< $node) > /dev/null
+  then
+    echo -e "${RED}Deleting node ${node}...${NC}"
+    multipass delete $node
+    multipass purge
+  fi
+  
+  echo -e "${BLUE}Launching ${node}. CPU: ${cpus}, MEM: ${ram}${NC}"
   multipass launch --disk $disk --memory $ram --cpus $cpus --name $node jammy
+  echo -e "${GREEN}$node booted!${NC}"
 done
 
+# Create host file entries
+echo -e "${BLUE}Provisioning...${NC}"
+hostentries=/tmp/hostentries
+[ -f hostentries ] && rm -f $hostentries
 
+# Add ips to /etc/hosts
+for spec in $(cat $vm_specs)
+do
+    node=$(cut -d ',' -f 1 <<< $spec)
+    ip=$(multipass info $node --format json | jq -r 'first( .info[] | .ipv4[0] )')
+    echo "$ip $node" >> $hostentries
+done
+
+# Set up nodes
+for spec in $(cat $vm_specs)
+do
+    node=$(cut -d ',' -f 1 <<< $vm_spec)
+    multipass transfer $hostentries $node:/tmp/
+    # multipass transfer $SCRIPT_DIR/01-setup-hosts.sh $node:/tmp/
+    # multipass transfer $SCRIPT_DIR/cert_verify.sh $node:/home/ubuntu/
+    # multipass exec $node -- /tmp/01-setup-hosts.sh
+done
+
+# Config CSR on the controlplane nodes
+multipass transfer $TOOLS_DIR/approve-csr.sh controlplane01:/home/ubuntu/
+
+echo -e "${GREEN}Done!${NC}"
